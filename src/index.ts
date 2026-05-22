b83c66d58df49b1718edbf5f24b27d6f86411e0b
mohit-761 <mohit761.mv@gmail.com>
2026-05-22 18:27:21 +0530

    start convero development

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import './config/env.config';
+import app from "./app";
+
+(() => {
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`SERVER IS RUNNING ON http://localhost:${PORT}`)
+    })
+})();
